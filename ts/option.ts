ee5c38abfd4c34b913c19272c27bed4c4f57dade
njhlai <nicholasjhlai@gmail.com>
2024-03-11 23:18:33 +0800

    Added option to use json for config modification

--- ts/option.ts
+++ ts/option.ts
@@ -2,6 +2,8 @@ import { configData, searchEngine, square } from "./types/configDataStructs.js";
 import { readDefaultConfig } from "./utils.js";
 
 // grab required HTMLElements
+const enableJSON = document.getElementById("enableJSON") as HTMLInputElement;
+const normal = document.getElementById("normal") as HTMLElement;
 const username = document.getElementById("username") as HTMLInputElement;
 const squares = document.getElementById("squares") as HTMLTextAreaElement;
 const enableSearch = document.getElementById("enableSearch") as HTMLInputElement;
@@ -22,6 +24,8 @@ const appid = document.getElementById("appid") as HTMLInputElement;
 const lat = document.getElementById("lat") as HTMLInputElement;
 const lon = document.getElementById("lon") as HTMLInputElement;
 const units = document.getElementById("units") as HTMLSelectElement;
+const json = document.getElementById("json") as HTMLElement;
+const config = document.getElementById("config") as HTMLTextAreaElement;
 
 // Parse and populate options form
 function populateOption(confData: configData) {
@@ -50,6 +54,8 @@ function populateOption(confData: configData) {
     lon.value = confData.weatherConf.lon;
     units.value = confData.weatherConf.units;
 
+    config.value = JSON.stringify(confData, null, 2);
+
     search.style.display = enableSearch.checked ? "flex" : "none";
     date.style.display = enableDate.checked ? "flex" : "none";
     weather.style.display = enableWeather.checked ? "flex" : "none";
@@ -58,6 +64,8 @@ function populateOption(confData: configData) {
 document.addEventListener("DOMContentLoaded", function () {
     const localData = localStorage.getItem("confData");
     localData ? populateOption(JSON.parse(localData) as configData) : readDefaultConfig(populateOption);
+
+    enableJSON.checked = false;
 });
 
 // Toggle search config options
@@ -75,38 +83,46 @@ enableWeather.addEventListener("change", function () {
     weather.style.display = enableWeather.checked ? "flex" : "none";
 });
 
+// Toggle config input method
+enableJSON.addEventListener("change", function () {
+    [normal.style.display, json.style.display] = [json.style.display, normal.style.display];
+});
+
 // Save current configuration
 document.querySelector("form")?.addEventListener("submit", function (e: SubmitEvent) {
     e.preventDefault();
 
-    const confData = {
-        username: username.value,
-        squares: JSON.parse(squares.value) as square[],
-        searchConf: {
-            enableSearch: enableSearch.checked,
-            searchEngines: JSON.parse(engines.value) as searchEngine[],
-        },
-        enableLocation: enableLocation.checked,
-        timeConf: {
-            enableDate: enableDate.checked,
-            enableTime: enableTime.checked,
-            locale: locale.value as Intl.LocalesArgument,
-            options: {
-                weekday: optionWeekday.value,
-                year: optionYear.value,
-                month: optionMonth.value,
-                day: optionDay.value,
-            } as Intl.DateTimeFormatOptions,
-        },
-        weatherConf: {
-            enableWeather: enableWeather.checked,
-            appid: appid.value,
-            lat: lat.value,
-            lon: lon.value,
-            units: units.value,
-        },
-    } as configData;
+    const confData = enableJSON.checked
+        ? (JSON.parse(config.value) as configData)
+        : {
+              username: username.value,
+              squares: JSON.parse(squares.value) as square[],
+              searchConf: {
+                  enableSearch: enableSearch.checked,
+                  searchEngines: JSON.parse(engines.value) as searchEngine[],
+              },
+              enableLocation: enableLocation.checked,
+              timeConf: {
+                  enableDate: enableDate.checked,
+                  enableTime: enableTime.checked,
+                  locale: locale.value as Intl.LocalesArgument,
+                  options: {
+                      weekday: optionWeekday.value,
+                      year: optionYear.value,
+                      month: optionMonth.value,
+                      day: optionDay.value,
+                  } as Intl.DateTimeFormatOptions,
+              },
+              weatherConf: {
+                  enableWeather: enableWeather.checked,
+                  appid: appid.value,
+                  lat: lat.value,
+                  lon: lon.value,
+                  units: units.value,
+              },
+          };
 
     console.log(JSON.stringify(confData, null, 2));
+    populateOption(confData);
     localStorage.setItem("confData", JSON.stringify(confData));
 });
